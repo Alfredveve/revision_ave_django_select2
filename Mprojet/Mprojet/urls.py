c3b27985ab40f85bb6f28dfb919527e1b06c3551
BEAVOGUI <vevebea2022@outlook.fr>
2024-07-02 23:51:00 +0000

    semi fini
    ici, je commence à la notion de branche github

--- Mprojet/Mprojet/urls.py
+++ Mprojet/Mprojet/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import index, addBlog
+from blog.views import index, addBlog, listblog, modifierblog
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='index'),
     path('create/', addBlog, name='create'),
+    path('table/', listblog, name='table'),
+    path('modifier/<int:pk>', modifierblog, name='modifier'),
     path('admin/', admin.site.urls),
 ]
 
--- Mprojet/blog/views.py
+++ Mprojet/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import BlogArticles
 from .forms import BlogArticlesForm
 
@@ -19,4 +19,22 @@ def addBlog(request):
         form.save()
         form = BlogArticlesForm()
         messages = "We have receive successfully an item"
-    return render(request, 'blog/create.html')
+    return render(request, 'blog/create.html', {'form': form, 'message': messages})
+
+def listblog(request):
+    blog = BlogArticles.objects.all()
+    context = {
+        'blogs' : blog
+    }
+    return render(request, 'blog/table.html', context)
+
+
+def modifierblog(request, pk):
+    obj = BlogArticles.objects.get(id=pk)
+    form = BlogArticlesForm(request.POST or None, request.FILES or None, instance = obj)
+    messages = ""
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        form = BlogArticles()
+        messages = "We have modified successfully"
+    return render(request, 'blog/modifier.html', {'form': form, 'message': messages})
